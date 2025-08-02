828696b30614a3d338780a841162f104edb2e5f4
Pruthviraj Rathod <prithvirathod29884@gmail.com>
2025-08-02 18:14:33 +0530

    floder strucher

---
+++ Template.py
@@ -0,0 +1,58 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+project_name = "textClassification"
+
+list_of_files = [
+    f"src/{project_name}/conponents/__init__.py",
+    f"src/{project_name}/conponents/data_intestion.py",
+    f"src/{project_name}/conponents/data_transforamation.py",
+    f"src/{project_name}/conponents/model_trainer.py",
+    f"src/{project_name}/conponents/model_evaluation.py",
+    f"src/{project_name}/conponents/model_pusher.py",
+    f"src/{project_name}/configuration/__init__.py"
+    f"src/{project_name}/configuration/gcloud_syncer.py",
+    f"src/{project_name}/constants/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/artifact_entity.py",
+    f"src/{project_name}/exception/__init__.py",
+    f"src/{project_name}/logger/__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/pipeline/train_pipeline.py",
+    f"src/{project_name}/pipeline/prediction_pipeline.py",
+    f"src/{project_name}/ml/__init__.py",
+    f"src/{project_name}/ml/model.py",
+    "app.py",
+    "demo.py",
+    "requirements.txt",
+    "Dockerfile",
+    "setup.py",
+    "dockerignore",
+    "README.md",
+
+
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    
+    else:
+        logging.info(f"{filename} is already exists")
