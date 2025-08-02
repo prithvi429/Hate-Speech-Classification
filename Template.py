4703e2ae4169c526150c04a0760061d80e8f8629
Pruthviraj Rathod <prithvirathod29884@gmail.com>
2025-08-02 23:47:50 +0530

    updates requriment

--- Template.py
+++ Template.py
@@ -5,19 +5,20 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "textClassification"
+project_name = "hate"
 
 list_of_files = [
-    f"{project_name}/conponents/__init__.py",
-    f"{project_name}/conponents/data_intestion.py",
-    f"{project_name}/conponents/data_transforamation.py",
-    f"{project_name}/conponents/model_trainer.py",
-    f"{project_name}/conponents/model_evaluation.py",
-    f"{project_name}/conponents/model_pusher.py",
-    f"{project_name}/configuration/__init__.py"
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_transforamation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
     f"{project_name}/configuration/gcloud_syncer.py",
     f"{project_name}/constants/__init__.py",
     f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
     f"{project_name}/entity/artifact_entity.py",
     f"{project_name}/exception/__init__.py",
     f"{project_name}/logger/__init__.py",
@@ -31,28 +32,26 @@ list_of_files = [
     "requirements.txt",
     "Dockerfile",
     "setup.py",
-    "dockerignore",
-    "README.md",
-
-
+    ".dockerignore"
 ]
+
+
 
 
 for filepath in list_of_files:
     filepath = Path(filepath)
+
     filedir, filename = os.path.split(filepath)
 
-    if filedir != "":
+    if filedir !="":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory:{filedir} for the file {filename}")
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
 
-    
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath,'w') as f:
+        with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
 
 
-    
     else:
         logging.info(f"{filename} is already exists")
